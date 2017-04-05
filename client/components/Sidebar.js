3c5f5c1cfcf2cf7d09ba401ba8906d0393d8611b
Jeffrey Ma <jjeffreyma@gmail.com>
2017-04-05 12:04:55 -0700

    Initial sidebar metrics average

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,17 +1,45 @@
 import React, { PropTypes } from 'react';
 
 const Sidebar = (props) => {
+  // props.selectedPlaylist.tracks is array of objects
+  const metricsHolder = {
+    valence: [],
+    danceability: [],
+    energy: []
+  };
+
+  props.selectedPlaylist.tracks.forEach(obj => {
+    metricsHolder.valence.push(obj.valence);
+    metricsHolder.danceability.push(obj.danceability);
+    metricsHolder.energy.push(obj.energy);
+  });
+
+  const calcAvg = (arr) => arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+
+  const metricsAvg = {
+    valence: calcAvg(metricsHolder.valence),
+    danceability: calcAvg(metricsHolder.danceability),
+    energy: calcAvg(metricsHolder.energy)
+  }
+
+  // console.log(metricsAvg);
 
   const playerUrl = (id, owner) => {
     console.log(owner);
-    return ("https://embed.spotify.com/?uri=spotify:user:").concat(owner).concat(":playlist:").concat(id); 
+    return ("https://embed.spotify.com/?uri=spotify:user:").concat(owner).concat(":playlist:").concat(id);
   }
   props.selectedPlaylist.id
   return (
     <div id="sidebar">
-      <img src="http://i.imgur.com/zlLcuiJ.png" id="spotify_logo"/>
-      <iframe src={playerUrl(props.selectedPlaylist.id, props.selectedPlaylist.owner)} width="100%" height="380" frameborder="0" allowtransparency="true" id = "player">
+      <img src="http://i.imgur.com/zlLcuiJ.png" id="spotify_logo" />
+      <iframe src={playerUrl(props.selectedPlaylist.id, props.selectedPlaylist.owner)} width="100%" height="380" frameBorder="0" allowTransparency="true" id="player">
       </iframe>
+      <div id="metrics">
+        <h3>Metrics Average</h3>
+        <p>Mood: {metricsAvg.valence.toFixed(3)}</p>
+        <p>Danceability: {metricsAvg.danceability.toFixed(3)}</p>
+        <p>Energy: {metricsAvg.energy.toFixed(3)}</p>
+      </div>
     </div>
   )
 }
